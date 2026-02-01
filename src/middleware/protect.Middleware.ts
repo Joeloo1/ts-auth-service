a4a88a103ed2f5a9bf363b1ed1794c01f0aec3db
joeloo1 <agboolajoel123@gmail.com>
2026-02-01 18:55:20 +0100

    feat(middleware): add protect middleware for route authentication

---
+++ src/middleware/protect.Middleware.ts
@@ -0,0 +1,79 @@
+import type { Request, Response, NextFunction } from "express";
+import { Types } from "mongoose";
+import { JwtPayload } from "jsonwebtoken";
+
+import User from "../model/userModel";
+import AppError from "../utils/AppError";
+import catchAsync from "../utils/catchAsync";
+import logger from "../config/logger";
+import { verifyAccessToken } from "../utils/jwt";
+
+interface ITokenPayload extends JwtPayload {
+  userId: Types.ObjectId;
+}
+
+// Extend Express Request interface to include 'user'
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
+
+const protect = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    // Get the Token and check if it's there
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      logger.warn("Unauthorizied user...");
+      return next(
+        new AppError(
+          "You are not logged in, Please log in to access this route",
+          400,
+        ),
+      );
+    }
+
+    // Verify Token
+    const decoded = verifyAccessToken(token) as ITokenPayload;
+
+    // Chwck if the user still exists
+    const currentUser = await User.findById(decoded.userId);
+    if (!currentUser) {
+      logger.warn("User with this token no longer exists");
+      return next(new AppError("User with this token no  longer exists", 401));
+    }
+
+    // Check if user changed password after token was issued
+    if (decoded.iat && currentUser.changedPasswordAfter(decoded.iat)) {
+      logger.warn("User recently changed password");
+      return next(
+        new AppError("You recently changed password, Please login again", 401),
+      );
+    }
+    req.user = currentUser;
+    next();
+  },
+);
+
+export const getAllUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await User.find();
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  },
+);
+
+export default protect;
