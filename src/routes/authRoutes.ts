b927e85e9dacd819b5815b55c03ef123f36f3fb7
joeloo1 <agboolajoel123@gmail.com>
2026-02-02 20:18:50 +0100

    feat: add logOut and refresh token routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,10 +2,15 @@ import { Router } from "express";
 
 import signUp from "../controllers/auth/signup";
 import login from "../controllers/auth/login";
+import logOut from "../controllers/auth/logout";
+import protect from "../middleware/protect.Middleware";
+import refreshToken from "../controllers/auth/refreshToken";
 
 const router = Router();
 
 router.route("/signup").post(signUp);
 router.route("/login").post(login);
+router.route("/logout").post(protect, logOut);
+router.route("/refresh-token").post(protect, refreshToken);
 
 export default router;
