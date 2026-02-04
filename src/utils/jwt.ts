37bca73f9ca13985f142463dc0b86c7f2050ab1e
joeloo1 <agboolajoel123@gmail.com>
2026-02-04 11:30:58 +0100

    feat: generate and verify email verification tokens using JWT

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,11 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 import { Types } from "mongoose";
 import config from "../config/config.env";
+
+interface TokenPayload extends JwtPayload {
+  email: string;
+}
 
 // Generate Access Token
 export const generateAccessToken = (userId: Types.ObjectId): string => {
@@ -24,4 +28,15 @@ export const verifyAccessToken = (token: string) => {
 
 export const verifyRefreshToken = (token: string) => {
   return jwt.verify(token, config.REFRESH_TOKEN_SECRET);
+};
+
+export const generateVerificationToken = (email: string): string => {
+  return jwt.sign({ email }, config.VERIFICATION_SECRET, {
+    expiresIn: "2h",
+  });
+};
+
+export const verifyToken = (token: string): TokenPayload => {
+  const decoded = jwt.verify(token, config.VERIFICATION_SECRET) as TokenPayload;
+  return decoded;
 };
